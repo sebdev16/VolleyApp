9d34cbd588ec934fa890237c4a8f47b80d0fc486
Sebastián Jasso <sebapplelife@gmail.com>
2024-12-09 10:52:27 -0600

    cambio del fondo y de las letras

--- app/src/main/java/com/example/volleyapp/MainActivity.kt
+++ app/src/main/java/com/example/volleyapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,6 +31,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,6 +69,7 @@ fun VolleyballScoreboard(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxSize()
+            .background(color = Color(0xFFE0F7FA))
             .padding(16.dp),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -95,8 +99,8 @@ fun VolleyballScoreboard(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = team1Name.text, fontSize = 24.sp)
-            Text(text = team1Score.toString(), fontSize = 48.sp, textAlign = TextAlign.Center)
+            Text(text = team1Name.text, fontSize = 24.sp, fontFamily = FontFamily.Monospace)
+            Text(text = team1Score.toString(), fontSize = 48.sp, textAlign = TextAlign.Center, fontFamily = FontFamily.Monospace)
         }
 
         Row(
@@ -106,8 +110,8 @@ fun VolleyballScoreboard(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = team2Name.text, fontSize = 24.sp)
-            Text(text = team2Score.toString(), fontSize = 48.sp)
+            Text(text = team2Name.text, fontSize = 24.sp, color = Color.Black, fontFamily = FontFamily.Monospace)
+            Text(text = team2Score.toString(), fontSize = 48.sp, color = Color.Black, fontFamily = FontFamily.Monospace)
         }
 //-------------------------------------------------------------------------------------------------------
 
